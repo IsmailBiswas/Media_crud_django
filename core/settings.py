035424a47f04d28fc60bc205b71438b324bdeb99
ismail <ismail@ismailbiswas.com>
2025-05-19 21:29:26 +0530

    add csrf trusted site

--- core/settings.py
+++ core/settings.py
@@ -124,3 +124,7 @@ MEDIA_ROOT = BASE_DIR
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://mediacruddjango-production.up.railway.app",
+]
